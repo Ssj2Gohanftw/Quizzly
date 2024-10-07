4f7acd1ea12fecac37154437228353450a397cf4
Ethan <ethanmdcosta@gmail.com>
2024-10-07 23:27:10 +0530

    Added sign out button in the settings page and fixed the bug with registration

--- app/src/main/java/com/example/quizapp/pages/ClassesPage.kt
+++ app/src/main/java/com/example/quizapp/pages/ClassesPage.kt
@@ -5,16 +5,27 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.quizapp.AuthState
 import com.example.quizapp.AuthViewModel
 
 @Composable
 fun ClassesPage(modifier: Modifier, navController: NavController,authViewModel: AuthViewModel) {
+    val authState = authViewModel.authState.observeAsState()
+    LaunchedEffect(authState.value) {
+        when (authState.value)
+        {
+            is AuthState.UnAuthenticated ->navController.navigate("login")
+            else->Unit
+        }
+    }
     Column(
         modifier =Modifier.fillMaxSize().background(Color(0xFF3F51B5)),
         verticalArrangement = Arrangement.Center,
--- app/src/main/java/com/example/quizapp/pages/SettingsPage.kt
+++ app/src/main/java/com/example/quizapp/pages/SettingsPage.kt
@@ -2,21 +2,40 @@ package com.example.quizapp.pages
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.quizapp.AuthState
 import com.example.quizapp.AuthViewModel
 
 @Composable
 fun SettingsPage(modifier: Modifier, navController: NavController, authViewModel: AuthViewModel) {
+    val authState = authViewModel.authState.observeAsState()
+    LaunchedEffect(authState.value) {
+        when (authState.value)
+        {
+            is AuthState.UnAuthenticated ->navController.navigate("login")
+            else->Unit
+        }
+    }
     Column(
-        modifier =Modifier.fillMaxSize().background(Color(0xFF3F51B5)),
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF3F51B5)),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -25,5 +44,12 @@ fun SettingsPage(modifier: Modifier, navController: NavController, authViewModel
             fontWeight =FontWeight.SemiBold,
             color = Color.White
         )
-    }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {authViewModel.signout()},
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF007BFF)),
+            modifier = Modifier.padding(16.dp)){
+            Text(text = "Sign out", color = Color.White)
+
+        }
+        }
 }
